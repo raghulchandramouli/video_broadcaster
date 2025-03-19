90528027530efb0c05b855c836108aa3b03d344d
Raghul <raghulchandramouli@gmail.com>
2025-03-19 19:03:29 +0530

    Video-Brodcaster Intgration closed

---
+++ main.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI, Query
+from fastapi.responses import JSONResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def serve_ui():
+    return FileResponse("static/index.html")
+
+@app.get("/devices")
+def abc():
+    pass
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
